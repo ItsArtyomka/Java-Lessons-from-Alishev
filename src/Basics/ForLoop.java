6723695e936344b5e6a5963ec4e0a7494119cff5
Artyom Pionov <pionovartem11@gmail.com>
2023-02-25 18:36:50 +0000

    First Commit

---
+++ src/Basics/ForLoop.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // This example prints evens from 2 to 20
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println("Number " + i + " is even!");
+        }
+    }
+}
---
+++ src/Basics/WhileLoops.java
@@ -0,0 +1,11 @@
+package Basics;
+@SuppressWarnings("ReassignedVariable")
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value < 10){
+            System.out.println(value += 2);
+        }
+    }
+}
