07cf14e07136b374fae5801d635c1080689bb942
Artyom Pionov <pionovartem11@gmail.com>
2023-02-25 22:37:29 +0000

    Second Commit

--- src/Basics/DoWhile.java
+++ src/Basics/DoWhile.java
@@ -1,5 +1,28 @@
 package Basics;
 
-public class DoWhile {
+import java.util.Scanner;
 
+
+@SuppressWarnings("ReassignedVariable")
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        /*int value = userInput.nextInt();
+        while (value != 5){
+            System.out.println("Enter '5'");
+            value = userInput.nextInt();
+        }
+        System.out.println("You have entered '5'");*/
+
+        // Both of the loops are identical, however one is shorter than the other
+
+        int value;
+        do {
+            System.out.println("Enter '5'");
+            value = userInput.nextInt();
+        } while (value != 5);
+        System.out.println("You have entered '5'");
+
+    }
 }
